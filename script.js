9e1bbc2e1eb2a9eeb739edc4fa308eea6aea12d7
cangos-dev <canga200000@mail.ru>
2026-03-23 16:14:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function addTask() {
   const li = document.createElement("li");
   li.textContent = text;
 
+  // Кнопка удаления
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";

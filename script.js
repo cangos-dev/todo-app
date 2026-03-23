bec1911efe743192e238bee4ac003538c02872b7
cangos-dev <canga200000@mail.ru>
2026-03-23 16:17:41 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert("JS подключен");
 function addTask() {
   const input = document.getElementById("taskInput");
   const text = input.value.trim();
@@ -6,18 +7,16 @@ function addTask() {
 
   const li = document.createElement("li");
 
-  // текст задачи
   const span = document.createElement("span");
   span.textContent = text;
 
-  // кнопка удаления
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = " ❌";
+
   deleteBtn.onclick = function () {
     li.remove();
   };
 
-  // собираем элемент
   li.appendChild(span);
   li.appendChild(deleteBtn);
 

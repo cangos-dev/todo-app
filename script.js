f2a4a4d787b264340b1e1a8bc47d19a0f08987b7
cangos-dev <canga200000@mail.ru>
2026-03-23 16:34:12 +0300

    script.js

--- script.js
+++ script.js
@@ -1,56 +1,80 @@
 const input = document.getElementById("taskInput");
 const list = document.getElementById("taskList");
 
-// загрузка задач
 window.onload = function () {
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.forEach(task => {
-    createTask(task);
+    createTask(task.text, task.completed);
   });
 };
 
-// добавление задачи
 function addTask() {
   const text = input.value.trim();
   if (text === "") return;
 
-  createTask(text);
-  saveTask(text);
+  createTask(text, false);
+  saveTask(text, false);
 
   input.value = "";
 }
 
-// создание элемента
-function createTask(text) {
+function createTask(text, completed) {
   const li = document.createElement("li");
 
   const span = document.createElement("span");
   span.textContent = text;
 
+  if (completed) {
+    span.style.textDecoration = "line-through";
+  }
+
+  // галочка
+  const checkBtn = document.createElement("button");
+  checkBtn.textContent = "✔️";
+  checkBtn.onclick = function () {
+    const isCompleted = span.style.textDecoration !== "line-through";
+
+    span.style.textDecoration = isCompleted ? "line-through" : "none";
+
+    updateTask(text, isCompleted);
+  };
+
+  // удаление
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = " ❌";
-
   deleteBtn.onclick = function () {
     li.remove();
     removeTask(text);
   };
 
+  li.appendChild(checkBtn);
   li.appendChild(span);
   li.appendChild(deleteBtn);
 
   list.appendChild(li);
 }
 
-// сохранить
-function saveTask(text) {
+function saveTask(text, completed) {
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.push(text);
+  tasks.push({ text, completed });
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// удалить
+function updateTask(text, completed) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks = tasks.map(task => {
+    if (task.text === text) {
+      return { text, completed };
+    }
+    return task;
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 function removeTask(text) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks = tasks.filter(t => t !== text);
+  tasks = tasks.filter(task => task.text !== text);
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }

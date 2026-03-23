ed1fec51afb633de7f5bb9d904e40cab107572ea
cangos-dev <canga200000@mail.ru>
2026-03-23 16:41:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
-alert("JS работает");
+const input = document.getElementById("taskInput");
+const list = document.getElementById("taskList");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// загрузка задач при старте
+window.onload = function () {
+  tasks.forEach(task => {
+    renderTask(task);
+  });
+};
+
+// добавление задачи
+function addTask() {
+  const text = input.value.trim();
+  if (text === "") return;
+
+  const task = {
+    id: Date.now(),
+    text: text,
+    completed: false
+  };
+
+  tasks.push(task);
+  saveTasks();
+  renderTask(task);
+
+  input.value = "";
+}
+
+// создание элемента
+function renderTask(task) {
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = task.text;
+
+  if (task.completed) {
+    span.style.textDecoration = "line-through";
+  }
+
+  // кнопка выполнено
+  const checkBtn = document.createElement("button");
+  checkBtn.textContent = "✔️";
+  checkBtn.onclick = function () {
+    task.completed = !task.completed;
+    saveTasks();
+    span.style.textDecoration = task.completed ? "line-through" : "none";
+  };
+
+  // кнопка удалить
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = " ❌";
+  deleteBtn.onclick = function () {
+    tasks = tasks.filter(t => t.id !== task.id);
+    saveTasks();
+    li.remove();
+  };
+
+  li.appendChild(checkBtn);
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+
+  list.appendChild(li);
+}
+
+// сохранение
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}

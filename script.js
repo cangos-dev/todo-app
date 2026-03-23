6df7776add6e52a8268466036fd9e400557ea465
cangos-dev <canga200000@mail.ru>
2026-03-23 16:16:00 +0300

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function addTask() {
   const input = document.getElementById("taskInput");
-  const text = input.value;
+  const text = input.value.trim();
 
   if (text === "") return;
 
   const li = document.createElement("li");
-  li.textContent = text;
 
-  // Кнопка удаления
+  // текст задачи
+  const span = document.createElement("span");
+  span.textContent = text;
+
+  // кнопка удаления
   const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "❌";
+  deleteBtn.textContent = " ❌";
   deleteBtn.onclick = function () {
     li.remove();
   };
 
+  // собираем элемент
+  li.appendChild(span);
   li.appendChild(deleteBtn);
+
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";

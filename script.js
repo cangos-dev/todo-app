a37fee1359e7dcf503b93e860cea570d00ca93a5
cangos-dev <canga200000@mail.ru>
2026-03-23 16:30:48 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+alert("JS работает");
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const text = input.value.trim();
+
+  if (text === "") return;
+
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = " ❌";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}

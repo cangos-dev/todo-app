ef8c4ce1afc3fc20ed5f40108d9fac08d6b150af
cangos-dev <canga200000@mail.ru>
2026-03-23 16:33:27 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-alert("JS работает");
+const input = document.getElementById("taskInput");
+const list = document.getElementById("taskList");
 
+// загрузка задач
+window.onload = function () {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach(task => {
+    createTask(task);
+  });
+};
+
+// добавление задачи
 function addTask() {
-  const input = document.getElementById("taskInput");
   const text = input.value.trim();
-
   if (text === "") return;
 
+  createTask(text);
+  saveTask(text);
+
+  input.value = "";
+}
+
+// создание элемента
+function createTask(text) {
   const li = document.createElement("li");
 
   const span = document.createElement("span");
@@ -13,14 +29,28 @@ function addTask() {
 
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = " ❌";
+
   deleteBtn.onclick = function () {
     li.remove();
+    removeTask(text);
   };
 
   li.appendChild(span);
   li.appendChild(deleteBtn);
 
-  document.getElementById("taskList").appendChild(li);
+  list.appendChild(li);
+}
 
-  input.value = "";
+// сохранить
+function saveTask(text) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(text);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// удалить
+function removeTask(text) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks = tasks.filter(t => t !== text);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
